68f9140248c16fe2edfc48c5ee2ee339e6de7064
sriram ilapakurty <sriram.in20@gmail.com>
2012-07-16 15:55:35 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Map::Application.config.secret_token = 'a48a31458f37b7f9239263e5f6743b4d74e4c305b8049c6b3ebf49577aaa345588e2a952bc55b376b0dfb0bd54e699dc20bf7c7e55e9143e037bbbb09348f1e0'
